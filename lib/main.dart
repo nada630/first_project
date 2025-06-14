b0e0a8c682384ae2a17d7edcc97bb45dae2cb343
nada630 <nadaashrafabdelohab@gmail.com>
2025-06-14 16:37:19 -0700

    Update UI elements in main application

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,23 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text("hello world ")),
-        appBar: AppBar(title: Text("nada"), backgroundColor: Colors.blue),
+        body: Center(
+          child: Container(
+            child: Text("markting"),
+            width: 150,
+            height: 150,
+            color: const Color.fromARGB(255, 243, 107, 33),
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("nada"),
+
+          backgroundColor: const Color.fromARGB(255, 221, 198, 234),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Icon(Icons.add),
-          backgroundColor: Colors.red,
+          backgroundColor: const Color.fromARGB(255, 211, 166, 196),
         ),
       ),
     );
