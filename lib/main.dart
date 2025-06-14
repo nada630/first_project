8515ee075fe43d8e9b11a5c03d945eb54206cd03
nada630 <nadaashrafabdelohab@gmail.com>
2025-06-14 12:24:40 -0700

    Add Windows runner files for Flutter application
    - Created `runner.exe.manifest` to define application settings and DPI awareness.
    - Implemented `utils.cpp` and `utils.h` for console creation, command line argument handling, and UTF-16 to UTF-8 string conversion.
    - Developed `win32_window.cpp` and `win32_window.h` to manage a high DPI-aware Win32 window, including theme updates and message handling.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  myApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text("hello world")),
+        appBar: AppBar(title: Text("nada"), backgroundColor: Colors.blue),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
